d88f34094694229ff74afc1cbe4724b813e3493d
Kyle Euman <euman_kyle@yahoo.com>
2021-04-17 11:39:31 -0400

    changed file connection and added more node modules and added the console table function

--- JS/connection.js
+++ JS/connection.js
@@ -1,12 +1,11 @@
 const mysql = require('mysql');
 
-connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: 'root',
     database: 'employee_trackerdb',
-    multipleStatements: true
 });
 
 module.exports = connection;
---
+++ SQL/seed.sql
@@ -0,0 +1,19 @@
+INSERT INTO departments(department_name)
+VALUES 
+
+
+
+
+
+
+INSERT INTO roles(title, salary, department_id)
+VALUES
+
+
+
+
+
+
+
+INSERT INTO employees(first_name, last_name, role_id) 
+VALUES
